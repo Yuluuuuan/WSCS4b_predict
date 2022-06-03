78ac0ad4da6d386e295ac3fc33fbe74fdd2f3449
Qi-pipi <90182888+Qi-pipi@users.noreply.github.com>
2022-06-03 03:41:25 +0200

    Update test_predict.py

--- test_predict.py
+++ test_predict.py
@@ -3,10 +3,13 @@ from predict import run_LR
 from predict import run_RF
 import unittest
 
+
 class TestStringMethods(unittest.TestCase):
     def test_visualization(self):
-        self.assertEqual(final_result('data','data'),"/data/allinfo_XGBoost_info.csv")
-        self.assertEqual(run_RF('data','data'),"/data/submission_RF.csv")
-        self.assertEqual(run_LR('data','data'),"/data/submission_LR.csv")
-if __name__=="__main__":
+        self.assertEqual(final_result('data', 'data'), "/data/allinfo_XGBoost_info.csv")
+        self.assertEqual(run_RF('data', 'data'), "/data/submission_RF.csv")
+        self.assertEqual(run_LR('data', 'data'), "/data/submission_LR.csv")
+
+
+if __name__ == "__main__":
     unittest.main()
